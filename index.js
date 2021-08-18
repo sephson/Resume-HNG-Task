bd4bd21b26b6b5bb31fb8d5f4d70454051489e99
sephson <disutj@gmail.com>
2021-08-18 04:56:08 +0100

    env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const sendMail = require("./mail");
@@ -8,10 +9,13 @@ app.use(express.json());
 
 app.post("/email", (req, res) => {
   console.log("Data", req.body);
-  const { subject, email, content } = req.body;
-  sendMail(email, subject, content, function (err, data) {
-    res.status(200).json({ data, message: "Email Sent" });
-    if (err) res.status(500).json({ message: "Error" });
+  const { subject, email, text } = req.body;
+  sendMail(email, subject, text, function (err, data) {
+    if (err) {
+      res.status(500).json({ message: "Error" });
+    } else {
+      res.status(200).json({ message: "Email Sent" });
+    }
   });
   //   res.status(200).json({ message: "yay" });
 });
@@ -20,7 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(3000, () => console.log("Server is running"));
 
-// process.on("unhandledRejection", (err) => {
-//   console.log(`Logged Error: ${err}`);
-//   server.close(() => process.exit(1));
-// });
+process.on("unhandledRejection", (err) => {
+  console.log(`Logged Error: ${err}`);
+  server.close(() => process.exit(1));
+});
--- mail.js
+++ mail.js
@@ -3,28 +3,33 @@ const mailGun = require("nodemailer-mailgun-transport");
 
 const auth = {
   auth: {
-    api_key: "db6eea6239fb6aafc6a53b56df7f6426-9776af14-bf7d1fb5",
-    domain: "sandboxdc83f4aebe8b49bab52a89307be65112.mailgun.org",
+    api_key: process.env.API_KEY,
+    domain: process.env.DOMAIN,
   },
 };
 
 const transporter = nodemailer.createTransport(mailGun(auth));
 
-const sendMail = (email, subject, content, callback) => {
+const sendMail = (email, subject, text, callback) => {
   const mailOptions = {
     from: email,
-    to: "disutj@gmail.com",
+    to: process.env.EMAIL,
     subject,
-    content,
+    text,
   };
 
   transporter.sendMail(mailOptions, function (err, data) {
     if (err) {
       //if error
+      console.log(err);
       return callback(err, null);
+    } else {
+      console.log("Message Sent!");
+      return callback(data, null);
     }
-    return cb(null, data);
   });
 };
+
+// console.log(process.env.API_KEY, process.env.DOMAIN, process.env.EMAIL);
 
 module.exports = sendMail;
